f9886016e670bb4da5b4d3eaadb94a4f2466c9a8
tabetommy <tabe_e@yahoo.ca>
2026-02-17 23:20:22 +0100

    feat: connexion backend locale incluse

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -2,17 +2,17 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const getApiUrl = () => {
-    // if (Platform.OS === 'web') {
-    //   return 'http://localhost:3000';
-    // } else if (Platform.OS === 'android') {
-    //   return 'http://192.168.0.176:3000';
-    // } else {
-    //   // iOS Simulator or Physical Device
-    //   return 'http://192.168.1.XX:3000'; // Replace with your IP
-    // }
+    if (Platform.OS === 'web') {
+      return 'http://localhost:3000';
+    } else if (Platform.OS === 'android') {
+      return 'http://192.168.0.176:3000';
+    } else {
+      // iOS Simulator or Physical Device
+      return 'http://192.168.1.XX:3000'; // Replace with your IP
+    }
 
-    //deployed backend url
-    return 'https://backend-production-9988.up.railway.app'
+    // //deployed backend url
+    // return 'https://backend-production-9988.up.railway.app'
   };
 
 
