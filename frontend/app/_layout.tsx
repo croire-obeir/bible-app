92699d8bd1fb956980519f29db37451b05705325
tabetommy <tabe_e@yahoo.ca>
2026-03-18 20:28:36 +0100

    feat: Ajout de lsg_1910.db et des configurations correspondantes

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,27 +3,41 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/use-color-scheme';
+//sqlite dependencies
+import { Suspense } from 'react'; 
+import { ActivityIndicator, View } from 'react-native';
+import { SQLiteProvider } from 'expo-sqlite'; 
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        {/* Écrans hors navigation par onglets (Auth) */}
-        <Stack.Screen name="screens/index" />
-        <Stack.Screen name="screens/Login" />
-        <Stack.Screen name="screens/Register" />
-       
-        {/* L'écran principal qui contiendra tes onglets */}
-        <Stack.Screen name="screens/(tabs)" />
-        <Stack.Screen name="Bible" />
-        <Stack.Screen name="Audios" />
-        <Stack.Screen name="Enseignements" />
-        <Stack.Screen name="Documents" />
-      </Stack>
+      {/* 1. Wrap everything in Suspense to handle the DB loading state */}
+      <Suspense fallback={
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      }>
+        {/* 2. Wrap the Stack in the SQLiteProvider */}
+        <SQLiteProvider 
+          databaseName="lsg_1910.db" 
+          assetSource={{ assetId: require('../assets/lsg_1910.db') }}
+          useSuspense
+        >
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="screens/index" />
+            <Stack.Screen name="screens/Login" />
+            <Stack.Screen name="screens/Register" />
+            <Stack.Screen name="screens/(tabs)" />
+            <Stack.Screen name="Bible" />
+            <Stack.Screen name="Audios" />
+            <Stack.Screen name="Enseignements" />
+            <Stack.Screen name="Documents" />
+          </Stack>
+        </SQLiteProvider>
+      </Suspense>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
